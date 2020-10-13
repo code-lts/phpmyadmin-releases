080760ae1164d98fe8dfde38ba69070d32445249
William Desportes <williamdes@wdes.fr>
2020-10-13 22:51:56 +0200

    Support for releases download

--- scripts/update-releases.sh
+++ scripts/update-releases.sh
@@ -6,24 +6,76 @@
 
 set -e
 
+# -- Functions -- #
+
+checkBinary () {
+	if ! command -v ${1} &> /dev/null
+	then
+		echo "${1} could not be found"
+		exit
+	fi
+}
+
+checkBinaries () {
+	checkBinary 'jq'
+	checkBinary 'curl'
+}
+
+getReleaseName () {
+   jq -r '.name' <<< "${1}"
+}
+
+getMajorReleases () {
+	jq -r '.[] | "\(.major).\(.minor)"' releases.json
+}
+
+getReleasesForMajorMinor () {
+	GET_RELEASES="\"\(.major).\(.minor)\"==\"${1}\""
+	jq -r ".[] | select(${GET_RELEASES})" releases.json | jq -c -r '.versions | .[]'
+}
+
+downloadReleaseFile () {
+	RELEASE_NAME="${1}"
+	RELEASE_VARIANT="${2}"
+	cd ./build/
+	if [ ! -f phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz ]; then
+		curl -# -O https://files.phpmyadmin.net/phpMyAdmin/${RELEASE_NAME}/phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz
+	fi
+	if [ ! -f phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz.asc ]; then
+		curl -# -O https://files.phpmyadmin.net/phpMyAdmin/${RELEASE_NAME}/phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz.asc
+	fi
+	if [ ! -f phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz.sha256 ]; then
+		curl -# -O https://files.phpmyadmin.net/phpMyAdmin/${RELEASE_NAME}/phpMyAdmin-${RELEASE_NAME}-${RELEASE_VARIANT}.tar.xz.sha256
+	fi
+	cd - > /dev/null
+}
+
+doImportOfVersionToCommit () {
+	RELEASE_NAME="$(getReleaseName "${version}")"
+	echo "Importing ${RELEASE_NAME} ..."
+	downloadReleaseFile "${RELEASE_NAME}" 'all-languages'
+	downloadReleaseFile "${RELEASE_NAME}" 'english'
+	downloadReleaseFile "${RELEASE_NAME}" 'source'
+}
+
+# -- Init -- #
+
+checkBinaries
 
 echo 'Fetch ...'
 #git fetch --all -p -P
 
-MAJOR_RELEASES=$(jq -r '.[] | "\(.major).\(.minor)"' releases.json)
-
-for majorMinor in $(echo -e ${MAJOR_RELEASES}); do
+for majorMinor in $(getMajorReleases); do
 echo 'Detected series:'
 echo "- ${majorMinor}"
 
-GET_RELEASES="\"\(.major).\(.minor)\"==\"${majorMinor}\""
+	echo 'Detected versions:'
+	for version in $(getReleasesForMajorMinor ${majorMinor}); do
+		echo "- $(getReleaseName "${version}")"
+	done
 
-VERSIONS=$(jq -r ".[] | select(${GET_RELEASES})" releases.json | jq -c -r '.versions | .[]')
-
-echo 'Detected versions:'
-for version in $(echo -e ${VERSIONS}); do
-	PATCH=$(jq '.patch' <<< "${version}")
-	echo "- ${majorMinor}.${PATCH}"
-done
-
+	echo 'Importing...'
+	for version in $(getReleasesForMajorMinor ${majorMinor}); do
+		doImportOfVersionToCommit "${version}"
+	done
 done
