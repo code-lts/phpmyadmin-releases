cb463737cf40bc1e80b37d0f1a5931d23ddcaec7
William Desportes <williamdes@wdes.fr>
2020-10-20 22:25:18 +0200

    Added new version: 4.8.0.1

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,7 +4,7 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticIniteb6c10d689fcd7d1810593900980374e
+class ComposerStaticInit4c3fa2315ef33315f7b6865cfc81d30a
 {
     public static $files = array (
         '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
@@ -165,10 +165,10 @@ class ComposerStaticIniteb6c10d689fcd7d1810593900980374e
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticIniteb6c10d689fcd7d1810593900980374e::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticIniteb6c10d689fcd7d1810593900980374e::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticIniteb6c10d689fcd7d1810593900980374e::$prefixesPsr0;
-            $loader->classMap = ComposerStaticIniteb6c10d689fcd7d1810593900980374e::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInit4c3fa2315ef33315f7b6865cfc81d30a::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit4c3fa2315ef33315f7b6865cfc81d30a::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit4c3fa2315ef33315f7b6865cfc81d30a::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit4c3fa2315ef33315f7b6865cfc81d30a::$classMap;
 
         }, null, ClassLoader::class);
     }
