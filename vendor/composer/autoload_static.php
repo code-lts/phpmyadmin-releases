7f6a434f195b20e7ee57e88d8d85e63cd46025ea
William Desportes <williamdes@wdes.fr>
2020-10-21 11:06:42 +0200

    Added new version: 4.7.9

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -4,10 +4,12 @@
 
 namespace Composer\Autoload;
 
-class ComposerStaticInit999704f2309bf5707604051454431ca3
+class ComposerStaticInitd32f002ef796223f70863cf686620a9d
 {
     public static $files = array (
+        '5255c38a0faeba867671b61dfda6d864' => __DIR__ . '/..' . '/paragonie/random_compat/lib/random.php',
         '0e6d7bf4a5811bfa5cf40c5ccd6fae6a' => __DIR__ . '/..' . '/symfony/polyfill-mbstring/bootstrap.php',
+        'bd9634f2d41831496de0d3dfe4c94881' => __DIR__ . '/..' . '/symfony/polyfill-php56/bootstrap.php',
         'decc78cc4436b1292c6c0d151b19445c' => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib/bootstrap.php',
     );
 
@@ -18,8 +20,11 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
         ),
         'S' => 
         array (
+            'Symfony\\Polyfill\\Util\\' => 22,
+            'Symfony\\Polyfill\\Php56\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Component\\ExpressionLanguage\\' => 37,
+            'Samyoul\\U2F\\U2FServer\\' => 22,
         ),
         'R' => 
         array (
@@ -27,9 +32,12 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
         ),
         'P' => 
         array (
+            'PragmaRX\\Google2FA\\Tests\\' => 25,
+            'PragmaRX\\Google2FA\\' => 19,
             'PhpMyAdmin\\SqlParser\\' => 21,
             'PhpMyAdmin\\ShapeFile\\' => 21,
             'PhpMyAdmin\\MoTranslator\\' => 24,
+            'ParagonIE\\ConstantTime\\' => 23,
             'PMA\\' => 4,
         ),
     );
@@ -39,6 +47,14 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
         array (
             0 => __DIR__ . '/..' . '/phpseclib/phpseclib/phpseclib',
         ),
+        'Symfony\\Polyfill\\Util\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-util',
+        ),
+        'Symfony\\Polyfill\\Php56\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/symfony/polyfill-php56',
+        ),
         'Symfony\\Polyfill\\Mbstring\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-mbstring',
@@ -47,9 +63,21 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
         array (
             0 => __DIR__ . '/..' . '/symfony/expression-language',
         ),
+        'Samyoul\\U2F\\U2FServer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/samyoul/u2f-php-server/src',
+        ),
         'ReCaptcha\\' => 
         array (
             0 => __DIR__ . '/..' . '/google/recaptcha/src/ReCaptcha',
+        ),
+        'PragmaRX\\Google2FA\\Tests\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/pragmarx/google2fa/tests',
+        ),
+        'PragmaRX\\Google2FA\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/pragmarx/google2fa/src',
         ),
         'PhpMyAdmin\\SqlParser\\' => 
         array (
@@ -63,9 +91,23 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
         array (
             0 => __DIR__ . '/..' . '/phpmyadmin/motranslator/src',
         ),
+        'ParagonIE\\ConstantTime\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/paragonie/constant_time_encoding/src',
+        ),
         'PMA\\' => 
         array (
             0 => __DIR__ . '/../..' . '/',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'B' => 
+        array (
+            'BaconQrCode' => 
+            array (
+                0 => __DIR__ . '/..' . '/bacon/bacon-qr-code/src',
+            ),
         ),
     );
 
@@ -89,9 +131,10 @@ class ComposerStaticInit999704f2309bf5707604051454431ca3
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit999704f2309bf5707604051454431ca3::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit999704f2309bf5707604051454431ca3::$prefixDirsPsr4;
-            $loader->classMap = ComposerStaticInit999704f2309bf5707604051454431ca3::$classMap;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitd32f002ef796223f70863cf686620a9d::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitd32f002ef796223f70863cf686620a9d::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitd32f002ef796223f70863cf686620a9d::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitd32f002ef796223f70863cf686620a9d::$classMap;
 
         }, null, ClassLoader::class);
     }
