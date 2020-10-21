243ddb8c077f4e209acf2b80fb3894ae55e168c5
William Desportes <williamdes@wdes.fr>
2020-10-21 11:02:40 +0200

    Added new version: 4.7.4

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitf375fa87e2093d8f1c8134104c2ce8cd::getLoader();
+return ComposerAutoloaderInit8276cfd74868925e84b4eaf5cfa75af1::getLoader();
