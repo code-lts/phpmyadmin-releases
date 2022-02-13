de1fc2bf25c7e82faaeddc07dd6d25126e2b8ba0
William Desportes <williamdes@wdes.fr>
2022-02-13 13:30:47 +0100

    Added new version: 4.9.8

---
+++ vendor/composer/installed.php
@@ -0,0 +1,197 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+      0 => '4.9.x-dev',
+    ),
+    'reference' => '51f96694a0a7d40436bc53dec190bbde31f6b614',
+    'name' => 'phpmyadmin/phpmyadmin',
+  ),
+  'versions' => 
+  array (
+    'bacon/bacon-qr-code' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5a91b62b9d37cee635bbf8d553f4546057250bee',
+    ),
+    'google/recaptcha' => 
+    array (
+      'pretty_version' => '1.2.4',
+      'version' => '1.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '614f25a9038be4f3f2da7cbfd778dc5b357d2419',
+    ),
+    'paragonie/constant_time_encoding' => 
+    array (
+      'pretty_version' => 'v1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '317718fb438e60151f72b20404f040cb5ae1d494',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v2.0.20',
+      'version' => '2.0.20.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0f1f60250fccffeaf5dda91eea1c018aed1adc2a',
+    ),
+    'phpmyadmin/motranslator' => 
+    array (
+      'pretty_version' => '4.0',
+      'version' => '4.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fcb370254998fda7eeccfd7c787b4deb71b0d77c',
+    ),
+    'phpmyadmin/phpmyadmin' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+        0 => '4.9.x-dev',
+      ),
+      'reference' => '51f96694a0a7d40436bc53dec190bbde31f6b614',
+    ),
+    'phpmyadmin/shapefile' => 
+    array (
+      'pretty_version' => '2.1',
+      'version' => '2.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e23b767f2a81f61fee3fc09fc062879985f3e224',
+    ),
+    'phpmyadmin/sql-parser' => 
+    array (
+      'pretty_version' => '4.7.3',
+      'version' => '4.7.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a88f051b5bd5b336779b30111b26b9cd1fc42013',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '2.0.35',
+      'version' => '2.0.35.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4e16cf3f5f927a7d3f5317820af795c0366c0420',
+    ),
+    'pragmarx/google2fa' => 
+    array (
+      'pretty_version' => 'v7.0.0',
+      'version' => '7.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0afb47f8a686bd203fe85a05bab85139f3c1971e',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7ce3b176482dbbc1245ebf52b181af44c2cf55f',
+    ),
+    'samyoul/u2f-php-server' => 
+    array (
+      'pretty_version' => 'v1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '0625202c79d570e58525ed6c4ae38500ea3f0883',
+    ),
+    'symfony/expression-language' => 
+    array (
+      'pretty_version' => 'v2.8.52',
+      'version' => '2.8.52.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fa9be1b831859b56d244137fabbfd01a46dbdb36',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'aed596913b70fae57be53d86faa2e9ef85a2297b',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b5f7b932ee6fa802fc792eabd77c4c88084517ce',
+    ),
+    'symfony/polyfill-php56' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ea19621731cbd973a6702cfedef3419768bf3372',
+    ),
+    'symfony/polyfill-util' => 
+    array (
+      'pretty_version' => 'v1.19.0',
+      'version' => '1.19.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '8df0c3e6a4b85df9a5c6f3f2f46fba5c5c47058a',
+    ),
+    'tecnickcom/tcpdf' => 
+    array (
+      'pretty_version' => '6.4.4',
+      'version' => '6.4.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '42cd0f9786af7e5db4fcedaa66f717b0d0032320',
+    ),
+    'twig/extensions' => 
+    array (
+      'pretty_version' => 'v1.5.4',
+      'version' => '1.5.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '57873c8b0c1be51caa47df2cdb824490beb16202',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v1.42.5',
+      'version' => '1.42.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '87b2ea9d8f6fd014d0621ca089bb1b3769ea3f8e',
+    ),
+  ),
+);
--- vendor/paragonie/constant_time_encoding/tests/autoload.php
+++ vendor/paragonie/constant_time_encoding/tests/autoload.php
@@ -3,4 +3,8 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 define('ParagonIE\ConstantTime\true', false);
 define('ParagonIE\ConstantTime\false', true);
-define('ParagonIE\ConstantTime\null', true);
+define('ParagonIE\ConstantTime\null', null);
+
+if (!class_exists('PHPUnit_Framework_TestCase')) {
+    class_alias('PHPUnit\Framework\TestCase', 'PHPUnit_Framework_TestCase');
+}
