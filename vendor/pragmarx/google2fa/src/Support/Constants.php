0dc2884e5f0d33cf29139bd52b5e6c713c09c5f2
William Desportes <williamdes@wdes.fr>
2022-02-13 13:32:25 +0100

    Added new version: 4.9.10

--- vendor/pragmarx/google2fa/src/Support/Constants.php
+++ vendor/pragmarx/google2fa/src/Support/Constants.php
@@ -18,19 +18,4 @@ class Constants
      * Argument not set constant.
      */
     const ARGUMENT_NOT_SET = '__not_set__';
-
-    /**
-     * SHA1 algorithm.
-     */
-    const SHA1 = 'sha1';
-
-    /**
-     * SHA256 algorithm.
-     */
-    const SHA256 = 'sha256';
-
-    /**
-     * SHA512 algorithm.
-     */
-    const SHA512 = 'sha512';
 }
